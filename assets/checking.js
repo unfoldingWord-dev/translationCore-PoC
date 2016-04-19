ca17f165bc76643b305d9c7868e066fda209ecdf
Christopher Klapp <christopher@klapp.name>
2016-04-19 13:17:58 -0400

    slight cleanup/organization

--- assets/checking.js
+++ assets/checking.js
@@ -108,4 +108,13 @@
     }
   };
 
+  Handlebars.registerHelper('figure_name', function(figure_id) {
+    return checking.figure_data[figure_id.data.key].name;
+  });
+
+  var source = $("#menu-template").html();
+  var template = Handlebars.compile( source );
+  $("#menu-placeholder").html( template( figures ) ); 
+  checking.update("metaphor", 1, 0);
+
 }(this, this.document));
--- assets/template.js
+++ assets/template.js
@@ -1,10 +1,3 @@
 (function (window, document) {
-  Handlebars.registerHelper('figure_name', function(figure_id) {
-    return checking.figure_data[figure_id.data.key].name;
-  });
 
-  var source = $("#menu-template").html();
-  var template = Handlebars.compile( source );
-  $("#menu-placeholder").html( template( figures ) ); 
-  checking.update("metaphor", 1, 0);
 }(this, this.document));
